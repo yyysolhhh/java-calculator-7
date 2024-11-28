1cdc462600f4b9d214157120ec14ad7586dc89fb
yyysolhhh <solbaram37@gmail.com>
2024-11-28 22:41:09 +0900

    feat(AppConfig): 싱글톤으로 구현

--- src/main/java/calculator/AppConfig.java
+++ src/main/java/calculator/AppConfig.java
@@ -1,9 +1,41 @@
 package calculator;
 
 import calculator.controller.CalculatorController;
+import calculator.domain.Delimiters;
+import calculator.service.DelimiterService;
+import calculator.service.NumberService;
 
 public class AppConfig {
+    private CalculatorController calculatorController;
+    private DelimiterService delimiterService;
+    private NumberService numberService;
+    private Delimiters delimiters;
+
     public CalculatorController calculatorController() {
-        return new CalculatorController();
+        if (calculatorController == null) {
+            calculatorController = new CalculatorController(delimiterService(), numberService());
+        }
+        return calculatorController;
+    }
+
+    public DelimiterService delimiterService() {
+        if (delimiterService == null) {
+            delimiterService = new DelimiterService(delimiters());
+        }
+        return delimiterService;
+    }
+
+    public NumberService numberService() {
+        if (numberService == null) {
+            numberService = new NumberService(delimiters);
+        }
+        return numberService;
+    }
+
+    public Delimiters delimiters() {
+        if (delimiters == null) {
+            delimiters = new Delimiters();
+        }
+        return delimiters;
     }
 }
