7812cf8ccd151655c64ce5ff381324bef4195509
yyysolhhh <solbaram37@gmail.com>
2024-11-28 22:47:08 +0900

    feat(domain): Numbers 일급 컬렉션 구현
    Numbers
    - 숫자를 List에 모으기 위한 클래스
    - addNumber 메서드로 List에 숫자 추가

---
+++ src/main/java/calculator/domain/Numbers.java
@@ -0,0 +1,15 @@
+package calculator.domain;
+
+import java.util.List;
+
+public class Numbers {
+    private List<Integer> numbers;
+
+    public Numbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public void addNumber(int number) {
+        numbers.add(number);
+    }
+}
